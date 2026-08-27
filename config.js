b3d0049bd3364fc00fb80d1e2e99ada764c1b733
P Omkareshwar Reddy <1014573+omkar2205@users.noreply.github.com>
2026-08-27 17:49:51 +0530

    Add Apps Script API config

---
+++ config.js
@@ -0,0 +1,6 @@
+window.UKVI_CONFIG = {
+  // Paste the deployed Google Apps Script Web App /exec URL here.
+  // Example: https://script.google.com/macros/s/AKfycb.../exec
+  API_URL: '',
+  BUILD: '20260827-sheets-api'
+};
